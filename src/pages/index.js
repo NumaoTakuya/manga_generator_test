fb4e14bc409f9bf3ec7e4a771f36efd0f72bb88c
Tyler <tyler.numao@climt.studio>
2023-05-03 02:42:40 +0900

    tail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,151 @@
-import React, { useRef, useState } from 'react';
-import * as faceapi from 'face-api.js';
-import Image from 'next/image';
+import React, { useState, useEffect } from "react";
+import * as faceapi from "face-api.js";
+import Image from "next/image";
+import { Button, Container, Grid, Typography } from "@mui/material";
+import ml5 from "ml5";
+
+const sources = [
+  "https://media.discordapp.net/ephemeral-attachments/1092492867185950852/1102974784884711514/3f07ed359ce9d67463dbf0a01c56071d.jpg?width=900&height=1060",
+  "https://media.discordapp.net/ephemeral-attachments/1092492867185950852/1102982059930161152/7f9e577686ec2e1e0700ff61232e208d.jpg?width=776&height=1060",
+  "https://media.discordapp.net/attachments/1058796281146908762/1102982726287626270/00004-659911599.png?width=1024&height=1024",
+  "https://media.discordapp.net/attachments/1061113259979178044/1102984309448646667/Screenshot_2023-05-02_at_12-19-44_Down_To_Earth_-_Episode_1.png?width=730&height=780",
+];
 
 const Playground = () => {
-  const [detections, setDetections] = useState([]);
-  const imgRef = useRef(null);
+  const [detections, setDetections] = useState(null);
+  const [sourceIndex, setSourceIndex] = useState(0);
 
-  const loadModels = async () => {
-    await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+  useEffect(() => {
+    loadModels();
+  }, []);
+
+  const loadModels = async () => { 
+    await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
   };
 
   const handleDetect = async () => {
-    if (!imgRef.current) return;
+    const imageElement = document.getElementById("face-image");
 
-    const detections = await faceapi.detectAllFaces(
-      imgRef.current,
-      new faceapi.SsdMobilenetv1Options()
-    );
+    if (!imageElement) return;
 
-    setDetections(detections);
+    const options =
+      detections === null
+        ? new faceapi.SsdMobilenetv1Options()
+        : new faceapi.TinyFaceDetectorOptions();
+
+    const detectionsWithLandmarks = await faceapi
+      .detectAllFaces(imageElement, options)
+      .withFaceLandmarks();
+
+    setDetections(detectionsWithLandmarks);
+  };
+
+  const handleNext = () => {
+    setSourceIndex((sourceIndex + 1) % sources.length);
+    setDetections(null);
   };
 
   const drawDetections = () => {
-    if (!imgRef.current) return;
+    if (!detections) return null;
 
-    const svg = (
-      <svg width={imgRef.current.width} height={imgRef.current.height}>
+    return (
+      <svg
+        width={640}
+        height={480}
+        style={{ position: "absolute", top: 0, left: 0 }}
+      >
         {detections.map((detection, i) => {
-          const { x, y, width, height } = detection.box;
+          const { x, y, width, height } = detection.detection.box;
+          const mouth = detection.landmarks.getMouth();
+
           return (
-            <rect
-              key={i}
-              x={x}
-              y={y}
-              width={width}
-              height={height}
-              stroke="red"
-              strokeWidth="2"
-              fill="none"
-            />
+            <g key={i}>
+              <rect
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                stroke="red"
+                strokeWidth="2"
+                fill="none"
+              />
+              <circle cx={mouth[14].x} cy={mouth[14].y} r="4" fill="blue" />
+            </g>
           );
         })}
       </svg>
     );
-
-    return svg;
   };
 
-  loadModels();
+  const renderDetections = () => {
+    if (!detections) return null;
+
+    return detections.map((detection, i) => {
+      const { x, y, width, height } = detection.detection.box;
+      const mouth = detection.landmarks.getMouth();
+      const mouthCoords = `(${mouth[14].x.toFixed(2)}, ${mouth[14].y.toFixed(
+        2
+      )})`;
+
+      return (
+        <div key={i}>
+          <Typography variant="h6" gutterBottom>
+            Detection {i + 1}
+          </Typography>
+          <Typography variant="body1">
+            Rectangle: x={x.toFixed(2)}, y={y.toFixed(2)}, width=
+            {width.toFixed(2)}, height={height.toFixed(2)}
+          </Typography>
+          <Typography variant="body1">
+            Mouth Coordinates: {mouthCoords}
+          </Typography>
+        </div>
+      );
+    });
+  };
 
   return (
-    <div>
-      <h1>Face Detection with SVG</h1>
-      <div style={{ position: 'relative' }}>
-        <div ref={imgRef} style={{ display: 'inline-block' }}>
-          <Image
-            src="https://media.discordapp.net/ephemeral-attachments/1092683500743831652/1102796655708864543/Screenshot_2023-05-02_at_12-19-44_Down_To_Earth_-_Episode_1.png?width=802&height=1060"
-            alt="face"
-            layout="responsive"
-            width={640}
-            height={480}
-          />
-        </div>
-        {drawDetections()}
-      </div>
-      <button onClick={handleDetect}>Detect Faces</button>
-    </div>
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Face Detection Test
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <div style={{ position: "relative" }}>
+            <div style={{ display: "inline-block" }}>
+              <Image
+                id="face-image"
+                src={sources[sourceIndex]}
+                alt="face"
+                layout="responsive"
+                width={640}
+                height={480}
+              />
+            </div>
+            {drawDetections()}
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          {renderDetections()}
+        </Grid>
+      </Grid>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleDetect}
+        sx={{ marginTop: 2 }}
+      >
+        Detect Faces
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleNext}
+        sx={{ marginTop: 2, marginLeft: 1 }}
+      >
+        Next Image
+      </Button>
+    </Container>
   );
 };
 
