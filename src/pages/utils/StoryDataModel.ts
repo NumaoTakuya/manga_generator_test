cbb2cbfbf767fb64da7ce9e71e5fdcaf75cdcd20
Tyler <tyler.numao@climt.studio>
2023-05-02 09:34:40 +0900

    DataFlow

--- src/pages/utils/StoryDataModel.ts
+++ src/pages/utils/StoryDataModel.ts
@@ -8,12 +8,12 @@ type Speaker = {
   appearance: string;
 };
 type Attributes = {
-  importance: number;
-  momentum: number;
-  intreval: number;
+  importance: number; // how important is this shot to the story
+  momentum: number; // how much is this shot dynamic
+  intreval: number; // how long it takes time between this shot and the next
 };
 type Description = {
-  description: string;
+  description: string; // description of the shot
 };
 
 type Shot = Dialogue & Attributes & Description;
