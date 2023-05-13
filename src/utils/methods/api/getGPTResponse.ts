f0e9d11be43180dc66918cc6e04f83c0fe7d8cf7
Tyler <tyler.numao@climt.studio>
2023-05-13 17:53:14 +0900

    updated getGPTResponse

--- src/utils/methods/api/getGPTResponse.ts
+++ src/utils/methods/api/getGPTResponse.ts
@@ -1,4 +1,4 @@
-import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai"; 
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
 type CompletionResponse = {
   content: string;
@@ -6,62 +6,37 @@ type CompletionResponse = {
   finishReason?: string;
 };
 
-const config = new Configuration({ 
-  apiKey: process.env.OPENAI_API_KEY,  
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(config);
-
 
 const getGPTResponse = async (
   prompt: string,
   messages: ChatCompletionRequestMessage[],
   model: string = "gpt-3.5-turbo",
   temperature?: number
-): Promise<Omit<CompletionResponse, "finishReason">> => {
-  let content: string | undefined = undefined;
-  const message: ChatCompletionRequestMessage = {
+): Promise<Omit<CompletionResponse, "finishReason">> => { 
+
+  const userMessage: ChatCompletionRequestMessage = {
     role: "user",
     content: prompt,
-  };
-  let count = 0;
-  while (typeof content === "undefined") {
-    count++;
-    if (count > 10) {
-      content = "Server error occurred. Please try again later.";
-      break;
-    } 
-    try {
-      const response = await openai.createChatCompletion({
-        model: model,
-        messages: [...messages, message],
-        temperature: temperature ?? 1,
-      });
-      const data = response.data.choices[0];
-      content = data.message?.content;
+  }; 
+  messages = [...messages, userMessage]
 
-      if (
-        data.finish_reason === "length" ||
-        typeof content === "undefined" ||
-        content.includes('"options"' || '"string"' || '""')
-      ) {
-        content = undefined;
-        continue;
-      }
-    } catch (e) {
-      console.error(e);
-      content = undefined;
-    }
-  }
-  messages.push({
-    role: "assistant",
-    content: content,
-  });
+  const gptResponse = await openai.createChatCompletion({
+    model: model,
+    messages: messages,
+    temperature: temperature ?? 1,
+  }); 
+  const gptMessage  = gptResponse.data.choices[0].message!; 
+  const content = gptMessage.content;
+  messages = [...messages, gptMessage];
 
   return {
     content,
     messages,
   };
-}; 
-
+};
 
 export default getGPTResponse;
