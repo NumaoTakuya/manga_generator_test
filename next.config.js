21036a50409214b2d5d50e1901f9e98087be9907
Tyler <tyler.numao@climt.studio>
2023-05-22 03:16:02 +0900

    removed env from nextConfig even comment out

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['media.discordapp.net'],
-  },
-  // env: { 
-  //   OPENAI_API_KEY: process.env.OPENAI_API_KEY, 
-  // },
+  }, 
 }
 
 module.exports = nextConfig
